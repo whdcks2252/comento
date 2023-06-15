a1ed61daabeeb329d7f0ef5400edbe356580381f
whdcks2252 <whdcks4569@naver.com>
2023-06-15 20:40:18 +0900

    test

--- settingweb/src/main/java/settingweb/Controller/HomeController.java
+++ settingweb/src/main/java/settingweb/Controller/HomeController.java
@@ -1,2 +1,14 @@
-package settingweb.Controller;public class HomeController {
+package settingweb.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("data", "Hello, Spring from IntelliJ! :)");
+        return "index";
+    }
 }
